24a33a5fa85a98c1aa7821705de7d49e8c4aa906
demetgit <demetkuzikk@gmail.com>
2023-01-15 14:16:28 +0300

    v2

--- force-app/main/default/aura/child1Aura/child1AuraController.js
+++ force-app/main/default/aura/child1Aura/child1AuraController.js
@@ -1,5 +1,6 @@
 ({
-    myAction : function(component, event, helper) {
+    childOnlickHandler : function(component, event, helper) {
 
+        component.set("v.childAttr","Yeni deger child controller dan geldi");
     }
 })
